1899d3803eb24d8a86b05cf61625ca5075516c14
ROHIT  RANA <83514950+rohittrana@users.noreply.github.com>
2026-09-24 06:53:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let firstName=document.getElementById("firstName");
+let lastName=document.getElementById("lastName");
+let phone=document.getElementById("phone");
+let email=Document.getElementById("email");
+let result=Document.getElementById("result");
+result.addEventListner('click',function(){
+	alert(
+		"First Name: " + firstName.value + "\n" +
+		"Last Name: " + lastName.value + "\n" +
+		"Phone: " + phone.value + "\n" +
+		"Email: " + email.value
+	);
+})
